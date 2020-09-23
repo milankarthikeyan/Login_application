e14ae6089f5025aa593a4b507b08acbbc2d132c2
priya <priya.chnskr@gmail.com>
2020-09-23 19:36:23 +0530

    second commit

--- app/src/main/java/com/example/loginapplication/ProfileActivity.java
+++ app/src/main/java/com/example/loginapplication/ProfileActivity.java
@@ -1,8 +1,7 @@
 package com.example.loginapplication;
 
 import android.content.Intent;
-//import android.support.annotation.NonNull;
-//import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,12 +23,26 @@ import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+//import android.support.annotation.NonNull;
+//import android.support.v7.app.AppCompatActivity;
+//import java.io.InputStreamReader;
+
 public class ProfileActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     Button logoutBtn;
     TextView userName,userEmail,userId;
     ImageView profileImage;
+
     private GoogleApiClient googleApiClient;
     private GoogleSignInOptions gso;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,12 +99,66 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
             });
         }
     }
+
+    private class Apicall extends AsyncTask<String, String, String>
+    {
+        final String Token1;
+        public Apicall(String Token) {
+
+                this.Token1=Token;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                String APIURL = "https://cardapi.kgetechnologies.com/api/token/GetTokenDataAsync?input="+Token1;
+                URL url = new URL(APIURL);
+                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Accept", "application/json");
+                //conn.setRequestProperty("Authorization", "Bearer"+Token1);
+
+                /*if (conn.getResponseCode() != 200) {
+                    throw new RuntimeException("Failed : HTTP Error code : "
+                            + conn.getResponseCode());
+                }*/
+                InputStreamReader in = new InputStreamReader(conn.getInputStream());
+                BufferedReader br = new BufferedReader(in);
+                String output;
+
+                while ((output = br.readLine()) != null) {
+                    userId.setText(output);
+                    //System.out.println(output);
+
+                }
+
+                conn.disconnect();
+
+            } catch (Exception e) {
+                            }
+            return null;
+        }
+
+
+        }
+
+
+
+
+
+
+
+
     private void handleSignInResult(GoogleSignInResult result){
         if(result.isSuccess()){
             GoogleSignInAccount account=result.getSignInAccount();
             userName.setText(account.getDisplayName());
             userEmail.setText(account.getEmail());
-            userId.setText(account.getIdToken());
+            Apicall asynctask=new Apicall(account.getIdToken());
+            asynctask.execute();
+
+
 
 
             try{
@@ -106,7 +173,7 @@ public class ProfileActivity extends AppCompatActivity implements GoogleApiClien
             gotoMainActivity();
         }
     }
-    
+
     private void gotoMainActivity(){
         Intent intent=new Intent(this,MainActivity.class);
         startActivity(intent);
